d596325ba00889782eb1d60c61dbcf9df1295e92
Victor Macari <victorm.macari@gmail.com>
2024-11-19 12:58:36 +0200

    fix: adds main method

--- selection-sort.py
+++ selection-sort.py
@@ -7,12 +7,16 @@ def find_smallest(arr):
             smallest_index = i
     return smallest_index
 
-
 def selection_sort(arr):
     new_arr = []
-    for i in range(len(arr)):
+    for _ in range(len(arr)):
         smallest = find_smallest(arr)
         new_arr.append(arr.pop(smallest))
     return new_arr
-    
-print(selection_sort([5,3,6,2,10]))
+
+def main(): 
+    my_list = [5,3,6,2,10]    
+    print(selection_sort(my_list))
+
+if __name__ == "__main__":
+    main()
